f47744ec82fe5f4c844ae68b50119f64618ce2e9
Abhijna Raghavendra <a_raghavendra@ma.iitr.ac.in>
2024-04-09 22:09:16 +0530

    feat: add zsh and oh my zsh

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -1,8 +1,16 @@
+echo "Execution started (zsh.sh)"
+
+# Installing zsh and oh my zsh
+sudo apt install zsh
+curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+chsh -s $(which zsh)
+
+# zsh config
 custom_config_url="https://raw.githubusercontent.com/mdgspace/server-setup/master/custom_config.zshrc"
 config_path="$HOME/.zshrc"
 custom_config="$HOME/custom_config.zshrc"
-
 wget -O $custom_config $custom_config_url
 chmod +x $custom_config
-
 echo "source $custom_config" | sudo tee -a "$config_path" > /dev/null
+
+echo "Execution completed (zsh.sh)"
