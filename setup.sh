6779283602b1de7fe58df51887b2345460a5c066
nik-55 <nik.xyz.in@gmail.com>
2024-04-12 17:44:16 +0530

    chore: add iam

--- setup.sh
+++ setup.sh
@@ -67,6 +67,20 @@ sudo certbot
 
 # region scripts/iam.sh
 sudo usermod -aG docker $USER
+sudo adduser stargazers
+sudo usermod -aG docker stargazers
+
+# Copy the public key to the new user
+sudo mkdir /home/stargazers/.ssh
+sudo chown stargazers:stargazers /home/stargazers/.ssh
+sudo chmod 700 /home/stargazers/.ssh
+
+echo "Provide the public key for non sudo user (stargazers)"
+read PUBLIC_KEY
+
+sudo sh -c "echo $PUBLIC_KEY >> /home/stargazers/.ssh/authorized_keys"
+sudo chown stargazers:stargazers /home/stargazers/.ssh/authorized_keys
+sudo chmod 600 /home/stargazers/.ssh/authorized_keys
 # endregion scripts/iam.sh
 
 # region scripts/webpage.sh
